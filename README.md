a84fe0713ed231ebba71e24a3f0ac3b4f507df17
Laura del Toro <lauradeltoro.arq@gmail.com>
2022-10-27 20:05:54 +0100

    Project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# the-canary-nomads-native
---
+++ android/app/src/main/java/com/contactsmanagerapp/MainApplication.kt
@@ -0,0 +1,36 @@
+package com.canarynomadsapp
+
+import android.app.Application
+
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.shell.MainReactPackage
+import com.facebook.soloader.SoLoader
+
+class MainApplication : Application(), ReactApplication {
+
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            return listOf<ReactPackage>(
+                MainReactPackage())
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+    }
+}
